0a804ba94ccbc8aeaf9c997c614ab1ffe3a7f56a
Алексей Валитов <alexeyvalitov28@gmail.com>
2024-05-01 15:16:41 +0300

    ok

--- mysum.go
+++ mysum.go
@@ -1,5 +1,36 @@
-package sum
+package arrint
 
-func Add(a, b int) int {
-	return a + b
+import (
+	"fmt"
+	"strings"
+)
+
+type MyStruct struct {
+	int
+}
+
+type ArrInt []int
+
+// Add складывает числа
+func Add(a, b ArrInt) ArrInt {
+	length := len(a)
+	if length-len(b) > 0 {
+		length = len(b)
+	}
+	c := make(ArrInt, length)
+	for i := 0; i < length; i++ {
+		c[i] = a[i] + b[i]
+	}
+	return c
+}
+
+// Метод String преобразует ArrInt в строку и возвращает её 223.
+func (a ArrInt) String() string {
+	out := make([]string,
+		len(a))
+
+	for i, v := range a {
+		out[i] = fmt.Sprintf(`<%d> `, v)
+	}
+	return fmt.Sprintf(`[%s]`, strings.Join(out, ` `))
 }
